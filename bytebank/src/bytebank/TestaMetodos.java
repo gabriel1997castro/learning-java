45e58dc784f592ffc638ab25d33b632ef7c638df
Gabriel Castro <gabriel.a.castro@unilever.com>
2021-12-13 09:59:10 -0300

    Composição de objetos

--- bytebank/src/bytebank/TestaMetodos.java
+++ bytebank/src/bytebank/TestaMetodos.java
@@ -2,28 +2,37 @@ package bytebank;
 
 public class TestaMetodos {
 	public static void main(String[] args) {
-		Conta primeiraConta = new Conta();
-		Conta segundaConta = new Conta();
-//		Conta segundaConta = primeiraConta;
-		
-		primeiraConta.saldo = 200;
-		System.out.println("Primeira conta " + primeiraConta.saldo);
-		primeiraConta.saldo += 100;
-		System.out.println("Primeira conta " + primeiraConta.saldo);
-		segundaConta.saldo = 50;
-		
-		System.out.println("Primeira conta tem " + primeiraConta.saldo);
-		System.out.println("Segunda conta tem " + segundaConta.saldo);
-		System.out.println("Depositando 600 na primeira");
-		primeiraConta.deposita(600);
-		System.out.println("Primeira conta tem " + primeiraConta.saldo);
-		System.out.println("Transferindo 100 para a segunda conta");
-//		primeiraConta.transfere(100, segundaConta);
-		if(primeiraConta.transfere(100, segundaConta)) {
-			System.out.println("Primeira conta tem " + primeiraConta.saldo);
-			System.out.println("Segunda conta tem " + segundaConta.saldo);
-		}
+//		Conta primeiraConta = new Conta();
+//		Conta segundaConta = new Conta();
+////		Conta segundaConta = primeiraConta;
+//		
+//		primeiraConta.saldo = 200;
+//		System.out.println("Primeira conta " + primeiraConta.saldo);
+//		primeiraConta.saldo += 100;
+//		System.out.println("Primeira conta " + primeiraConta.saldo);
+//		segundaConta.saldo = 50;
+//		
+//		System.out.println("Primeira conta tem " + primeiraConta.saldo);
+//		System.out.println("Segunda conta tem " + segundaConta.saldo);
+//		System.out.println("Depositando 600 na primeira");
+//		primeiraConta.deposita(600);
+//		System.out.println("Primeira conta tem " + primeiraConta.saldo);
+//		System.out.println("Transferindo 100 para a segunda conta");
+////		primeiraConta.transfere(100, segundaConta);
+//		if(primeiraConta.transfere(100, segundaConta)) {
+//			System.out.println("Primeira conta tem " + primeiraConta.saldo);
+//			System.out.println("Segunda conta tem " + segundaConta.saldo);
+//		}
 		// Como pode ser visto primeiraConta e segundaConta não são objetos
 		// mas sim referências.
+		
+		teste();
+	}
+	
+	public static void teste() {
+		Conta contaDoJoao = new Conta();
+		contaDoJoao.titular = new Cliente();
+		contaDoJoao.titular.nome = "João";
+		System.out.println("Conta do " + contaDoJoao.titular.nome);
 	}
 }
